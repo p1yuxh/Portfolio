a27ecba91ed7e0d5709a054b6dcb8e03d4e3fa1c
p1yuxh <94751985+p1yuxh@users.noreply.github.com>
2021-11-21 14:01:23 +0530

    Add files via upload

---
+++ Portfolio.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+                .contact
+        {
+          border: 0px;
+          width: 500px;
+          font-size: large;
+          font-family: cursive;
+          font-weight: bold;
+          color: chartreuse;
+          background-color: rgb(255, 136, 136);
+        }
+    </style>
+</head>
+<body style="background-color: black;color: aliceblue;">
+  <style>
+      *{ text-decoration: none;
+        
+    }
+  </style>  
+    <center>
+        
+    <h1 style="display: inline-flex;color: darkorchid;">My Portfolio</h1> 
+           
+        <table border="0"cellspacing="25" cell spacing="10">
+            <tr>
+                <td>
+                    <font face="cursive" size="5"><a href="#home"></a> Home</font>
+                </td>
+                <td>|</td>
+                <td>
+                    <font face="cursive" size="5"><a href="#about"></a> About</font>
+                </td>
+                <td>|</td>
+                <td>
+                    <font face="cursive" size="5"><a href="#project"></a> Project</font>
+                </td>
+                <td>|</td>
+                <td>
+                    <a href="#contact" style="text-decoration:none">
+                        <font face="Verdana" size="5" color=white>
+                            Contact
+                        </font>
+                    </a>
+                </td>
+            </tr>
+        </table>
+    </center> 
+    
+    <center style="background-color: antiquewhite;color: black;"> 
+        <h2>About</h2>
+    <table>
+        <tr>
+      <td>
+            <img src="Piyush.jpeg" alt="" height="400" width="300">
+        </td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+    <td>
+        <center>
+     <font style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;"> Hello Viwers <br>I am Piyush Dratt Panda <br>I am from Giet University ,Gunupur <br>I am a Web Devloper.</center></font>
+    </td></tr>
+    </table></center> 
+    <table id="contact" border="0" width="100%"
+    cellpadding="0" cellspacing="0" bgcolor="#c2c0c3">
+    <tr>
+        <td>
+            <table border="0" cellpadding="15"
+                cellspacing="0" width="80%" align="center">
+                <tr>
+                    <td height="180" align="center"
+                        valign="middle" colspan="2">
+                        <font face="Verdana" size="7"
+                            color="black">
+                            Contact
+                        </font>
+                        <hr color="black" width="90">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align="center" valign="top">
+                        <table border="0" width="50%" cellpadding="15"
+                            cellspacing="0" align="center" bgcolor="black">
+                            <tr>
+                                <td width="30%">
+                                    <hr color="black">
+                                    <font face="Verdana" size="4"
+                                        color="#ffffff">
+                                        Name
+                                    </font>
+                                </td>
+                                <td width="70%">
+                                    <font face="Verdana" size="4"
+                                        color="#ffffff">
+                                        <input type="text" size="40">
+                                    </font>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td width="30%">
+                                    <font face="Verdana" size="4"
+                                        color="#ffffff">
+                                        Email
+                                    </font>
+                                </td>
+                                <td width="70%">
+                                    <font face="Verdana" size="4"
+                                        color="#ffffff">
+                                        <input type="email" size="40">
+                                    </font>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td width="30%">
+                                    <font face="Verdana" size="4"
+                                        color="#ffffff">
+                                        Number
+                                    </font>
+                                </td>
+                                <td width="70%">
+                                    <font face="Verdana" size="4"
+                                        color="#ffffff">
+                                        <input type="number" size="12">
+                                    </font>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td width="30%">
+                                    <font face="Verdana" size="4"
+                                        color="#ffffff">
+                                        Message
+                                    </font>
+                                </td>
+                                <td width="70%">
+                                    <font face="Verdana" size="4"
+                                        color="#ffffff">
+                                        <textarea rows="5"
+                                            cols="37">
+                                        </textarea>
+                                    </font>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td width="30%">
+                                    
+                                </td>
+                                <td width="70%">
+                                    <button type="Submit">
+                                        <font face="Verdana"
+                                            size="4" color="black">
+                                            <b>Submit</b>
+                                        </font>
+                                    </button>
+                                    <hr color="black">
+                                    <hr color="black">
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        
+                        
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+    <table id="footer" border="0" width="100%"
+		cellpadding="0" cellspacing="0" bgcolor="#4CAF50">
+		<tr>
+			<td>
+				<table border="0" cellpadding="15"
+					cellspacing="0" width="90%" align="center">
+					<tr>
+						<td width="13%" valign="top">
+							<b>LinkedIn</b>
+							<a href="https://www.linkedin.com/in/piyush-dratt-panda-045055225" style="text-decoration:none">
+								➲
+							</a>
+						</td>
+
+						<td>
+							|
+						</td>
+
+						<td width="13%" valign="top">
+							<b>GitHub</b>
+							<a href="https://github.com/p1yuxh" style="text-decoration:none">
+								➲
+							</a>
+						</td>
+						<td>
+							|
+						</td>
+
+						<td width="13%" valign="top">
+							<b>Facebook</b>
+							<a href="https://www.facebook.com/vk.piyush.7" style="text-decoration:none">
+								➲
+							</a>
+						</td>
+
+						<td>
+							|
+						</td>
+
+						<td width="13%" valign="top">
+							<b>Twitter</b>
+							<a href="https://twitter.com/thescorp10x?t=Y2Cc6AkmpYoSyJAc9c9RKQ&s=08" style="text-decoration:none">
+								➲
+							</a>
+						</td>
+
+						<td>
+							|
+						</td>
+
+						<td width="13%" valign="top">
+							<b>Instagram</b>
+							<a href="https://instagram.com/p1yush.01"text-decoration:none">
+								➲
+							</a>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+    <!-- <br><br><br><br><center> -->
+    
+     <!-- <div class="contact"> -->
+          <!-- <h3>Contact</h3>  <br> -->
+     <!-- <a href="https://www.linkedin.com/in/piyush-dratt-panda-045055225">LinkedIn</a> |        <a href="https://www.facebook.com/vk.piyush.7">Facebook</a> |        <a href="https://www.instagram.com/p1yush.01">Instagram</a> |        <a href="https://github.com/p1yuxh">Github</a> |        <a href="https://twitter.com/thescorp10x?t=Y2Cc6AkmpYoSyJAc9c9RKQ&s=08">Twitter</a>  </div> -->
+<!-- </center> -->
+    </body>
+</html>
